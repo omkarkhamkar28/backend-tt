2dca0c9891b9a8958d4cf0ad5b88e5dc94c6a695
Omkar Khamkar <omkarkhamkar2004@gmail.com>
2025-10-07 09:13:26 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,28 +4,29 @@ dotenv.config();
 
 const con = async () => {
     //local database
-    try {
-        let url = `mongodb://${process.env.DB_HOST}:${process.env.MONGOPORT}/${process.env.DB_NAME}`;
-
-        await mongoose.connect(url, {
-            useNewUrlParser : true,
-        });
-
-        console.log(`connected To Mongodb database : ${process.env.DB_NAME}`);
-    } catch (error) {
-        console.error(error.message);
-        process.exit();
-    }
-
-    //Online database
     // try {
-    //     const conn = await mongoose.connect(process.env.MONGO_URL)
-    //     console.log(`connected To Mongodb database ${conn.connection.host}`);
+    //     let url = `mongodb://${process.env.DB_HOST}:${process.env.MONGOPORT}/${process.env.DB_NAME}`;
+
+    //     await mongoose.connect(url, {
+    //         useNewUrlParser : true,
+    //     });
+
+    //     console.log(`connected To Mongodb database : ${process.env.DB_NAME}`);
     // } catch (error) {
     //     console.error(error.message);
-    //     console.log("Database connecting error.")
     //     process.exit();
     // }
+
+    //Online database
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL)
+        console.log(`connected To Mongodb database ${conn.connection.host}`);
+    } catch (error) {
+        console.error(error.message);
+        console.log("Database connecting error.")
+        process.exit();
+    }
 }
+
 
 module.exports = con;
